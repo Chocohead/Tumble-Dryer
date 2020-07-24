d21a69530765bc60874abd03f96c0779270d4a09
Erlend Åmdal <erlend@aamdal.com>
2020-07-24 10:05:14 +0200

    Change from 4 spaces to tabs

--- src/com/chocohead/clm/client/ClothesLineMotorClient.java
+++ src/com/chocohead/clm/client/ClothesLineMotorClient.java
@@ -13,8 +13,8 @@ import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.render.RenderLayer;
 
 public class ClothesLineMotorClient implements ClientModInitializer {
-    @Override
-    public void onInitializeClient() {
-        BlockRenderLayerMap.INSTANCE.putBlock(ClothesLineMotor.MOTOR, RenderLayer.getCutout());
-    }
+	@Override
+	public void onInitializeClient() {
+		BlockRenderLayerMap.INSTANCE.putBlock(ClothesLineMotor.MOTOR, RenderLayer.getCutout());
+	}
 }
--- src/com/chocohead/clm/client/RichInteractions.java
+++ src/com/chocohead/clm/client/RichInteractions.java
@@ -13,15 +13,15 @@ import com.jamieswhiteshirt.clothesline.api.client.RichInteractionRegistry;
 import com.jamieswhiteshirt.clothesline.api.client.RichInteractionType;
 
 public class RichInteractions implements RichInteractionRegistry.Consumer {
-    @Override
-    public void accept(RichInteractionRegistry registry) {
-        registry.addBlock(ClothesLineMotor.MOTOR, (state, world, pos, player, hitResult) -> {
-            MotorBlock.Status status = state.get(MotorBlock.STATUS);
-            if (status == MotorBlock.Status.ON || status == MotorBlock.Status.OFF) {
-                MotorBlock.Rotation interactionRotation = MotorBlock.getInteractionRotation(pos, hitResult.getPos().x, hitResult.getPos().z, player);
-                return interactionRotation == MotorBlock.Rotation.CLOCKWISE ? RichInteractionType.ROTATE_CLOCKWISE : RichInteractionType.ROTATE_COUNTER_CLOCKWISE;
-            }
-            return RichInteractionType.NONE;
-        });
-    }
+	@Override
+	public void accept(RichInteractionRegistry registry) {
+		registry.addBlock(ClothesLineMotor.MOTOR, (state, world, pos, player, hitResult) -> {
+			MotorBlock.Status status = state.get(MotorBlock.STATUS);
+			if (status == MotorBlock.Status.ON || status == MotorBlock.Status.OFF) {
+				MotorBlock.Rotation interactionRotation = MotorBlock.getInteractionRotation(pos, hitResult.getPos().x, hitResult.getPos().z, player);
+				return interactionRotation == MotorBlock.Rotation.CLOCKWISE ? RichInteractionType.ROTATE_CLOCKWISE : RichInteractionType.ROTATE_COUNTER_CLOCKWISE;
+			}
+			return RichInteractionType.NONE;
+		});
+	}
 }
