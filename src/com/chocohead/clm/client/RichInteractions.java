1e0d9dc25ef7102f1e8fff9ac787eacc11e1e71d
Erlend Åmdal <erlend@aamdal.com>
2020-07-24 09:55:53 +0200

    Update license headers

--- src/com/chocohead/clm/client/RichInteractions.java
+++ src/com/chocohead/clm/client/RichInteractions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Chocohead
+ * Copyright 2020 Chocohead
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
